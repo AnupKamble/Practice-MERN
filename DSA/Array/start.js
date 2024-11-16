53b1057c21ba6500e8362ca5e18625d969606309
AnupKamble <sahyadrihillsagro@gmail.com>
2024-11-16 22:59:25 +0530

    day 2

---
+++ DSA/Array/start.js
@@ -0,0 +1,64 @@
+
+const arr = [1,2,3,4,5,6,7,8,9]
+ 
+
+
+//  ----------- Push and pop -----------
+//  Push to add the element in array in last and return a new array
+// Pop to remove the element from last amd returns new array
+
+
+arr.push(10);
+arr.pop();
+
+// console.log(arr)
+// -----------------------------------------------------------------------------
+
+//  ----------- shift and pop -----------
+//  Shift to rempve th first element in array and return a new array
+// Pop to remove the element from last amd returns new array
+//    console.log(arr)
+
+
+
+// --------- find --------------
+
+// let num;
+
+function checknum() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function checkNum() {
+   
+    // let sumNum = arr.find()
+// for (var i=0; i<arr.length; i++) {
+
+//   if (arr[i] >= 5) {
+//     console.log(arr[i])
+//   }
+//   else {
+//     console.log("sorry not found")
+//   }
+// }
+
+
+    
+}
+
+checkNum();
+// console.log(arr);
+
